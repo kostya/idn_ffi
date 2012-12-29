295c884b56e2083b4e425b76504db01f4084a48a
Konstantin Makarchev <kostya27@gmail.com>
2012-12-29 15:39:57 +0400

    # combine methods

--- lib/idn_ffi/methods.rb
+++ lib/idn_ffi/methods.rb
@@ -16,29 +16,16 @@ private
 
   UTF8 = "UTF-8"
 
-  if RUBY_VERSION >= "1.9"
-    def transform(method, str, flags)
-      return unless str
-      pointer = FFI::MemoryPointer.new :pointer
-      int = IDNFFI::Lib.send(method, str, pointer, flags)
-      raise IDNFFI::Error, "bad answer from libidn #{int}, http://www.gnu.org/software/libidn/reference/libidn-idna.html#IDNA-SUCCESS:CAPS" if int != 0
-      strptr = pointer.read_pointer()
-      result = strptr.read_string()
-      pointer.free
-      result.force_encoding(UTF8)
-      result                                      
-    end
-  else
-    def transform(method, str, flags)
-      return unless str
-      pointer = FFI::MemoryPointer.new :pointer
-      int = IDNFFI::Lib.send(method, str, pointer, flags)
-      raise IDNFFI::Error, "bad answer from libidn #{int}, http://www.gnu.org/software/libidn/reference/libidn-idna.html#IDNA-SUCCESS:CAPS" if int != 0
-      strptr = pointer.read_pointer()
-      result = strptr.read_string()
-      pointer.free
-      result
-    end                                          
+  def transform(method, str, flags)
+    return unless str
+    pointer = FFI::MemoryPointer.new :pointer
+    int = IDNFFI::Lib.send(method, str, pointer, flags)
+    raise IDNFFI::Error, "bad answer from libidn #{int}, http://www.gnu.org/software/libidn/reference/libidn-idna.html#IDNA-SUCCESS:CAPS" if int != 0
+    strptr = pointer.read_pointer()
+    result = strptr.read_string()
+    pointer.free
+    result.force_encoding(UTF8) if result.respond_to?(:force_encoding)
+    result                                      
   end
 
 end
